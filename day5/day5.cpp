72d67cdf171fe1f4532deb896d66ad5a937b70c6
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2022-12-09 22:13:21 +0200

    Add day5

---
+++ day5/day5.cpp
@@ -0,0 +1,106 @@
+#include <cstdlib>
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+#include <fstream>
+using std::ifstream;
+#include <set>
+using std::set;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+#include <cmath>
+using std::max;
+#include <algorithm>
+using std::back_inserter;
+using std::sort;
+
+const char *INPUT_FILE = "input.txt";
+
+struct Instruction {
+    uint32_t amount;
+    uint32_t from;
+    uint32_t to;
+};
+
+void addIf(const char *str, size_t index, vector<vector<char> > &stacks) {
+    if (str[0] == '[') {
+        stacks.at(index).push_back(str[1]);
+    }
+}
+
+int main() {
+   ifstream iFile(INPUT_FILE);
+    if (!iFile.is_open()) {
+        return EXIT_FAILURE;
+    }
+
+    vector<vector<char> > stacks;
+
+    string str;
+    while (getline(iFile, str)) {
+        istringstream ss(str);
+
+        // Init stacks the first time
+        if (stacks.size() == 0) {
+            for (uint32_t i = 0; i < std::ceil(str.size() / 4.); i++) {
+                stacks.push_back({});
+            }
+        }
+
+        char buffer[4];
+
+        size_t index = 0;
+        while (ss.good()) {
+            ss.read(buffer, 3);
+            cout << buffer << endl;
+            addIf(buffer, index, stacks);
+
+            // Skip ' '
+            if (ss.good()) {
+                ss.read(buffer, 1);
+            }
+
+            index++;
+        }
+
+        if (iFile.peek() == '\n') {
+            break;
+        }
+    }
+
+    for (auto i = stacks.begin(); i != stacks.end(); i++) {
+        std::reverse(i->begin(), i->end());
+    }
+
+    vector<Instruction> instructions;
+    // Skip one line
+    getline(iFile, str);
+
+    while (getline(iFile, str)) {
+        Instruction instr;
+        std::sscanf(str.c_str(), "move %u from %u to %u", &instr.amount, &instr.from, &instr.to);
+        instructions.push_back(instr);
+    }
+
+
+    for (auto instr : instructions) {
+        for (uint32_t i = instr.amount; i > 0; i--) {
+            cout << "from " << instr.from << " to " << instr.to << endl;
+            stacks.at(instr.to - 1).push_back(stacks.at(instr.from - 1).back());
+            stacks.at(instr.from - 1).pop_back();
+        }
+    }
+
+    for (auto i : stacks) {
+        cout << i.back();
+    }
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
