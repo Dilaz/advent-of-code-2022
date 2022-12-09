816e6033e3d8317a6ca62f47fb070cf0fd8c4fba
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2022-12-09 22:13:46 +0200

    Add template

---
+++ template/dayX.cpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+#include <fstream>
+using std::ifstream;
+#include <set>
+using std::set;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+#include <cmath>
+using std::max;
+#include <algorithm>
+using std::back_inserter;
+using std::sort;
+
+const char *INPUT_FILE = "input.txt";
+
+int main() {
+   ifstream iFile(INPUT_FILE);
+    if (!iFile.is_open()) {
+        return EXIT_FAILURE;
+    }
+
+    string str;
+    while (getline(iFile, str)) {
+    }
+
+
+    return EXIT_SUCCESS;
+}
